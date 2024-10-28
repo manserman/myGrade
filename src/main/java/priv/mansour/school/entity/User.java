b152077682d90cf886f3efdc4b24662d14cb4cf4
manslit <mansourh923@gmail.com>
2024-10-28 21:57:08 +0100

    User

--- src/main/java/priv/mansour/school/entity/User.java
+++ src/main/java/priv/mansour/school/entity/User.java
@@ -1,16 +1,39 @@
 package priv.mansour.school.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @Entity
 public abstract class User {
-
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
 	private String nom;
 	private String prenom;
-	Role role;
+	private Role role;
 	public User(String nom, String prenom, Role role) {
-		this.nom=nom;
-		this.prenom=prenom;
-		this.role=role;
+		this.setNom(nom);
+		this.setPrenom(prenom);
+		this.setRole(role);
+	}
+	public Role getRole() {
+		return role;
+	}
+	public void setRole(Role role) {
+		this.role = role;
+	}
+	public String getPrenom() {
+		return prenom;
+	}
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
 	}
 }
