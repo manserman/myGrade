acac830684745370bfc4d2de95e7eb89da520d63
manslit <mansourh923@gmail.com>
2025-02-28 16:42:43 +0100

    Student controller  refacdtor

--- src/main/java/priv/mansour/school/controller/StudentController.java
+++ src/main/java/priv/mansour/school/controller/StudentController.java
@@ -5,16 +5,12 @@ import static priv.mansour.school.utils.Constants.STUDENT;
 import java.util.List;
 
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import priv.mansour.school.entity.Student;
 import priv.mansour.school.logger.GlobalLogger;
@@ -32,38 +28,50 @@ public class StudentController {
     }
 
     @PostMapping
-    public ResponseEntity<Student> addStudent(@RequestBody Student student) {
-        return ResponseEntity.ok(studentService.add(student));
+    public ResponseEntity<Student> addStudent(@Valid @RequestBody Student student) {
+        GlobalLogger.infoCreate(STUDENT, "Creating new student: " + student.getId());
+        Student createdStudent = studentService.add(student);
+        GlobalLogger.infoSuccess("Created", STUDENT, "Student ID: " + createdStudent.getId());
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdStudent);
     }
 
     @GetMapping
     public ResponseEntity<List<Student>> getAllStudents() {
         GlobalLogger.infoReadAll(STUDENT);
-        return ResponseEntity.ok(studentService.getAll());
+        List<Student> students = studentService.getAll();
+        GlobalLogger.infoSuccess("Fetched all", STUDENT, "Total students: " + students.size());
+        return ResponseEntity.ok(students);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Student> getStudentById(@PathVariable String id) {
-        GlobalLogger.infoRead(STUDENT, id);
-        return ResponseEntity.ok(studentService.getById(id));
+    public ResponseEntity<Student> getStudentById(@PathVariable @NotBlank String id) {
+        GlobalLogger.infoRead(STUDENT, "Fetching student by ID: " + id);
+        Student student = studentService.getById(id);
+        GlobalLogger.infoSuccess("Fetched by ID", STUDENT, "Student ID: " + student.getId());
+        return ResponseEntity.ok(student);
     }
 
     @GetMapping("/by-mail/{mail}")
-    public ResponseEntity<Student> getStudentByMail(@PathVariable String mail) {
-        GlobalLogger.infoRead(STUDENT, mail);
-        return ResponseEntity.ok(studentService.findByEmail(mail));
+    public ResponseEntity<Student> getStudentByMail(@PathVariable @Email String mail) {
+        GlobalLogger.infoRead(STUDENT, "Fetching student by Email: " + mail);
+        Student student = studentService.findByEmail(mail);
+        GlobalLogger.infoSuccess("Fetched by Email", STUDENT, "Student Email: " + mail);
+        return ResponseEntity.ok(student);
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Student> updateStudent(@PathVariable String id, @Valid @RequestBody Student updatedStudent) {
-        GlobalLogger.infoUpdate(STUDENT, id, updatedStudent);
-        return ResponseEntity.ok(studentService.update(id, updatedStudent));
+    public ResponseEntity<Student> updateStudent(@PathVariable @NotBlank String id, @Valid @RequestBody Student studentToUpdate) {
+        GlobalLogger.infoUpdate(STUDENT, id, studentToUpdate);
+        Student updatedStudent = studentService.update(id, studentToUpdate);
+        GlobalLogger.infoSuccess("Updated", STUDENT, " ID: " + id);
+        return ResponseEntity.ok(updatedStudent);
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteStudentById(@PathVariable String id) {
-        GlobalLogger.infoDelete(STUDENT, id);
+    public ResponseEntity<Void> deleteStudentById(@PathVariable @NotBlank String id) {
+        GlobalLogger.infoDelete(STUDENT, "Deleting student ID: " + id);
         studentService.deleteById(id);
+        GlobalLogger.infoSuccess("Deleted", STUDENT, "Student ID: " + id);
         return ResponseEntity.noContent().build();
     }
 }
